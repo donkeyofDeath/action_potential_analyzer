ae875a38bfabdd3409caa4d06df6fbd8fc5c41e9
Tobias Liebmann <tobias.liebmann@gmx.de>
2021-04-30 21:30:01 +0200

    Restored the project.

--- data_analyzer.py
+++ data_analyzer.py
@@ -1,13 +1,33 @@
 import numpy as np
-import scipy as sp
+# import scipy as sp
+import scipy.signal as sig
 import pandas as pd
+# import stfio
 
 
 class DataAnalyzer:
+    """
 
-    def __init__(self, data_50khz):
+    """
+
+    def __init__(self, action_potential_data, sampling_freq, resample_freq):
         """
 
         """
-        self.data_50khz = data_50khz
-        self.data_100khz = 
+        self.original_data = action_potential_data
+        self.sampling_freq = sampling_freq
+        self.resample_freq = resample_freq
+        self.resample_data = sig.resample(self.original_data, self.sampling_freq, self.resample_freq)
+
+    @staticmethod
+    def resample(data, original_freq, desired_freq):
+        """
+
+        :param data:
+        :param original_freq:
+        :param desired_freq:
+        :return:
+        """
+        number_of_data_points = len(data)
+        number_of_resampled_data_points = number_of_data_points * desired_freq / original_freq
+        return sig.resample_poly(data, number_of_resampled_data_points, number_of_data_points)
