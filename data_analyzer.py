67392f89c609e1baccb06186dd860cdfad1c3712
Tobias Liebmann <tobias.liebmann@gmx.de>
2021-04-30 02:32:16 +0200

    Started adding the data analyzer class.

---
+++ data_analyzer.py
@@ -0,0 +1,13 @@
+import numpy as np
+import scipy as sp
+import pandas as pd
+
+
+class DataAnalyzer:
+
+    def __init__(self, data_50khz):
+        """
+
+        """
+        self.data_50khz = data_50khz
+        self.data_100khz = 
